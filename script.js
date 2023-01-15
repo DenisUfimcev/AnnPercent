a8dfb9e09cb2d444d673997bb2efa8bcc3e60ba2
DenisUfimcev <31904284+DenisUfimcev@users.noreply.github.com>
2023-01-15 12:53:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,77 +2,56 @@
 
 alert('Привет Асюнь, я твой личный помощник меня зовут "КВД", буду рад помочь тебе с любыми вычислениями, но пока я только могу посчитать твою зарплату, надеюсь в будущем я смогу больше;)')
 
-function machine() {
-let numberSell = prompt('На какую сумму в среднем за день продано?');
-let percentSell = prompt('Сколько % забранных?');
-let percentYour = prompt('Сколько % ты зарабатываешь с них?');
-
-  if (isNaN(numberSell)) {
-    alert('Извини но это не число: ' + numberSell + '\n' + 'Вводи пожалуйста только числа, КВД их любит');
-  } else if (isNaN(percentSell)) {
-    alert('Извини но это не число: ' + percentSell + '\n' + 'Вводи пожалуйста только числа, КВД их любит');
-  } else if (isNaN(percentYour)) {
-    alert('Извини но это не число: ' + percentYour + '\n' + 'Вводи пожалуйста только числа, КВД их любит');
+function accept() {
+  if(confirm('Ух какую большую цифру я тут насчитал, это точно твоё?')) {
+    alert("Твоя зарплата: " + main.sum());
   } else {
-    let resultMounts = numberSell * 22;
-  let centralPercentTake = ( resultMounts / 100 ) * percentSell;
-  let centralYourPercent = ( centralPercentTake / 100 ) * percentYour;
-
-alert('Итого всего продано за месяц на сумму: ' + resultMounts + 'грн' + '\n' + 
-  'Из них забрали на сумму: ' + centralPercentTake + 'грн' + '\n' + 
-  'Твой заработок с учётом забраных ' + percentSell + '%' + ' будет состовлять: ' + centralYourPercent + 'грн');
-
-function ask(question, yes, no) {
-  if(confirm(question)) yes()
-    else no();
+    alert('Я тоже думаю что ты не заслужила столько :p')
+    doublAccept()
+  }
+};
+function doublAccept() {
+  if(confirm('Или все таки заслужила?;)')) {
+    alert('Тогда вот так: ' + main.sum());
+  } 
 }
-
-function showNext() {
-  alert('Твоя ставка 36грн за час/ рабочий день 8 часов = 288грн за день');
-  let daysWork = prompt('Сколько дней в месяце ты отработала?', '22');
-  let salaryStack = daysWork * 288;
-  let salary = salaryStack + centralYourPercent;
-  alert('Твой заработок ставки в месяц составляет: ' + salaryStack);
-  alert('Если к ней добавить процент который мы вычисляли ранее то выходит полная зарплата: ' + '\n' + 
-    salary + 'грн' + ' В месяц (' + daysWork + ' рабочих дня)');
-  alert('Спасибо что воспользовались мной, приходи ещё' + '\n' + 'Твой "КВД"')
-}
-
-function showCancel() {
-  alert('Ну и ладно ;(');
-}
-
-ask('Хотите посчитать полную сумму зарплаты?', showNext, showCancel);
+function acceptTwo() {
+  if(confirm('Хочешь посчитать полную зарплату?')) {
+    main.questionTwo();
+  } else {
+    alert('Ну и как хотиш');
   }
 }
-machine();
 
-  let resultMounts = numberSell * 22;
-  let centralPercentTake = ( resultMounts / 100 ) * percentSell;
-  let centralYourPercent = ( centralPercentTake / 100 ) * percentYour;
+let main = {
+  numberSell: this.numberSell,
+  percentSell: this.percentSell,
+  percentYour: this.percentYour,
+  howmuchDays: this.howmuchDays,
+  salaryStack: this.salaryStack,
+  salary: this.salary,
+  question() {
+    this.numberSell = +prompt('На какую сумму в среднем за день продано?');
+    this.percentSell = +prompt('Сколько % забранных?');
+    this.percentYour = +prompt('Сколько % ты зарабатываешь с них?');
+    this.howmuchDays = +prompt('Сколько рабочих дней у тебя было за это время?');
+  },
+  questionTwo() {
+    alert('Твоя ставка 35грн за час / рабочий день 8 часов = 280грн за день');
+    alert('Как ты сказала ты отработала ' + this.howmuchDays + " дня");
+    this.salaryStack = this.howmuchDays * 280;
+    this.salary = this.salaryStack + this.sum();
+    alert('Твой заработок ставки в месяц составляет: ' + this.salaryStack );
+    alert('Если к ней добавить процент который мы вычисляли ранее ' + this.sum() + ' то выходит полная зарплата: ' + '\n' + 
+    this.salary + 'грн' + ' В месяц (' + this.howmuchDays + ' рабочих дней)');
+    alert('Спасибо что воспользовались мной, приходи ещё' + '\n' + 'Твой "КВД"')
+  },
+  sum() {
+   return (((this.numberSell * this.howmuchDays / 100) * this.percentSell) / 100) * this.percentYour;
+  }
+};
 
-alert('Итого всего продано за месяц на сумму: ' + resultMounts + 'грн' + '\n' + 
-  'Из них забрали на сумму: ' + centralPercentTake + 'грн' + '\n' + 
-  'Твой заработок с учётом забраных ' + percentSell + '%' + ' будет состовлять: ' + centralYourPercent + 'грн');
-
-function ask(question, yes, no) {
-  if(confirm(question)) yes()
-    else no();
-}
-
-function showNext() {
-  alert('Твоя ставка 36грн за час/ рабочий день 8 часов = 288грн за день');
-  let daysWork = prompt('Сколько дней в месяце ты отработала?', '22');
-  let salaryStack = daysWork * 288;
-  let salary = salaryStack + centralYourPercent;
-  alert('Твой заработок ставки в месяц составляет: ' + salaryStack);
-  alert('Если к ней добавить процент который мы вычисляли ранее то выходит полная зарплата: ' + '\n' + 
-    salary + 'грн' + ' В месяц (' + daysWork + ' рабочих дня)');
-  alert('Спасибо что воспользовались мной, приходи ещё' + '\n' + 'Твой "КВД"')
-}
-
-function showCancel() {
-  alert('Ну и ладно ;(');
-}
-
-ask('Хотите посчитать полную сумму зарплаты?', showNext, showCancel);
+main.question();
+main.sum();
+accept();
+acceptTwo();
